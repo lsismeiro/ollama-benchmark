ac9d59e5212c5b63e5a7339e31ba62443023eb1a
Jeff Geerling <geerlingguy@mac.com>
2024-10-28 14:29:08 -0500

    Simplify the benchmark a bit.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # ollama-benchmark
-Linux Bash script that sets CPU governor to Performance, takes User input on Ollama Model and number of times to run benchmark. Does not identify if CPU or GPU, but does provide a Tokens per Second (tokens/s) for eval rate. Usually sets CPU governor back to system default. Clears Model from Vram once complete.  
+
+This bash script benchmarks ollama on any system where it is installed.
+
+For a quick installation, try:
+
+```
+curl -fsSL https://ollama.com/install.sh | sh
+```
+
+Verify you can run `ollama` with a given model:
+
+```
+export ollama_model="llama3.2:3b"
+ollama run $ollama_model
+```
+
+Then run this benchmark script:
+
+```
+./obench.sh
+```
+
+## Limitations
+
+Currently the script just outputs the data from three runs. There are some TODO's in the script for things I'd like to make nicer someday, for now I just want a quick way to run the same tests on different machines :)
+
+I may also set something up where I could run the tests against [Open WebUI](https://docs.openwebui.com) so it could also do an end-to-end test of how well a host would perform hosting a chatbot UI for multiple users. Probably not. But maybe.
+
+## Findings
+
+| System | Model | Eval Rate |
+| --- | --- | --- |
+| Raspberry Pi 5 - 8GB | llama3.2:3b | 4.35 Tokens/s |
+| Raspberry Pi 5 - 8GB | llama3.1:8b | TODO |
+
+## Further Reading
+
+This script is just a quick way of comparing _one aspect_ of generative AI performance. There are _many other_ aspects that are as important (or more important) this script does _not_ cover.
+
+See [All about Timing: A quick look at metrics for LLM serving](https://isaac-chung.github.io/blog/llm-serving) for a good overview of other metrics you may want to compare when running Ollama.
+
+## Author
+
+This benchmark is based on the upstream project [tabletuser-blogspot/ollama-benchmark](https://github.com/tabletuser-blogspot/ollama-benchmark), and is maintained by Jeff Geerling.
--- obench.sh
+++ obench.sh
@@ -1,76 +1,26 @@
 #!/bin/bash
 # Benchmark using ollama gives rate of tokens per second
 # idea taken from https://taoofmac.com/space/blog/2024/01/20/1800
-# other colors
-#Black      	0;30	Dark Gray   	1;30
-#Red        	0;31	Light Red   	1;31
-#Green      	0;32	Light Green   1;32
-#Brown/Orange 0;33  	Yellow      	1;33
-#Blue       	0;34	Light Blue  	1;34
-#Purple     	0;35	Light Purple  1;35
-#Cyan       	0;36	Light Cyan  	1;36
-#Light Gray   0;37  	White       	1;37
-#ANSI option
-#RED='\033[0;31m'
-#NC='\033[0m' # No Color
-#echo -e "${red}Hello Stackoverflow${NC}"
-#set -e used for troubleshooting
+
 set -e
-#colors available
-borange='\e[0;33m'
-yellow='\e[1;33m'
-purple='\e[0;35m'
-green='\e[0;32m'
-red='\e[0;31m'
-blue='\e[0;34m'
-NC='\e[0m' # No Color
-cpu_def=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-echo "Setting cpu governor to"
-sudo echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-gpu_avail=$(sudo lshw -C display | grep product: | head -1 | cut -c17-)
-cpugover=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-cpu_used=$(lscpu | grep 'Model name' | cut -f 2 -d ":" | awk '{$1=$1}1')
-echo ""
-echo "Simple benchmark using ollama and"
-echo "whatever local Model is installed."
-echo "Does not identify if $gpu_avail is benchmarking"
-echo ""
+
+# TODO: Default options for ease of use, like `./obench.sh -d`.
 echo "How many times to run the benchmark?"
 read benchmark
-echo ""
-echo -e "Total runs "${purple}$benchmark${NC}
-echo ""
+
 echo "Current models available locally"
 echo ""
 ollama list
 echo ""
-echo "Example enter tinyllama or dolphin-phi"
+echo "Enter model you'd like to run (e.g. llama3.2)"
 echo ""
 read model
-ollama show $model --system
 echo ""
-echo -e "Will use model: "${green}$model${NC}
-echo ""
-echo -e Will benchmark the tokens per second for ${cpu_used} and or ${gpu_avail}
-touch "${cpu_used}".txt
-echo "" > "${cpu_used}".txt
-echo ""
-echo -e Running benchmark ${purple}$benchmark${NC} times for ${cpu_used} and or ${gpu_avail}
-echo -e with ${borange}$cpugover${NC} setting for cpu governor
+
+echo -e "Running benchmark "$benchmark" times using model: "$model
 echo ""
 for run in $(seq 1 $benchmark);
-do echo "Why is the blue sky blue?" | ollama run $model --verbose 2>&1 >/dev/null | grep "eval rate:" | tee -a "${cpu_used}".txt ;
-
-avg=$(cat "${cpu_used}".txt | grep -v "prompt eval rate:" | awk '{print $3}' | awk 'NR>1{ tot+=$1 } END{ print tot/(NR-1) }')
+do echo "Why is the blue sky blue?" | ollama run $model --verbose 2>&1 >/dev/null | grep "eval rate:" ;
 done
-echo ""
-echo -e ${red}$avg${NC} is the average ${blue}tokens per second${NC} using ${green}$model${NC} model
-echo for $cpu_used and or $gpu_avail
-echo -e using ${borange}$cpugover${NC} for cpu governor.
-echo ""
-echo "Setting cpu governor to"
-sudo echo $cpu_def | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-#comment this out if you are repeating the same model
-#this clears model from Vram
-sudo systemctl stop ollama; sudo systemctl start ollama
-#EOF
+
+# TODO: Average the three results, maybe still display them as they are output.
